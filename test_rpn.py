bfa163fcab314d292de34cdf0325e8fb55775240
thejman <thejman@umich.edu>
2017-10-31 23:26:31 -0400

    Added tests for exponentiation

--- test_rpn.py
+++ test_rpn.py
@@ -15,3 +15,15 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
+    def test_exp_regular(self):
+        result = rpn.calculate(3 5 ^)
+        self.assertEqual(243, result)
+    def test_exp_0(self):
+        result = rpn.calculate("5 0 ^")
+        self.assertEqual(1, result)
+    def test_exp_neg(self):
+        result = rpn.calculate("2 -2 ^")
+        self.assertEqual(-0.25, result)
+    def test_exp_frac(self):
+        result = rpn.calculate("9 0.5 ^")
+        self.assertEqual(3, result)
